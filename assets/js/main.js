4daacf401a0047e0f84e3dc5a553e556e2b7b21d
gabriele <gabrielescodinu96@gmail.com>
2020-11-12 15:47:38 +0100

    commit 1

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+// prompt
+var userName = prompt("Nome?");
+var userSurname = prompt("Cognome?");
+var userColor = prompt("Colore preferito?");
+
+// result
+document.getElementById('result').innerHTML = userName + userSurname +userColor + "20";
